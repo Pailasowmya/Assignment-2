7704abe3fbb2e29afdc18170be39db1ee24dfd94
Pailasowmya <69006691+Pailasowmya@users.noreply.github.com>
2020-10-21 08:35:27 +0530

    Add files via upload

---
+++ Cir.java
@@ -0,0 +1,13 @@
+package assign2;
+
+import assign2.Circle;
+
+public class Cir {
+		
+		public static void main(String[] args) {
+			Circle circle = new Circle(8.0);
+			System.out.println(Math.ceil(circle.getArea()));
+		}
+
+	}
+
